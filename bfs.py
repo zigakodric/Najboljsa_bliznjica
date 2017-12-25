b20684d5ca51f22268517eff5f327ff2451f8aab
zigakodric <kodric@hotmail.com>
2017-12-26 00:14:00 +0100

    Pov

--- bfs.py
+++ bfs.py
@@ -1,10 +1,8 @@
-graph = {'A': ['D'],
-         'B': ['D'],
-         'C': ['D',"G"],
-         'D': ['A', "B", "C", "E"],
-         'E': ['D',"F"],
-         'F': ['D'],
-         'G': ['C']}
+graph = {"A": ["B", "D"],
+         "B": ["A","C"],
+         "C": ["B"],
+         "D": ["A","E"],
+         "E": ["D"]}
 
 k = list(graph.keys())
 
@@ -49,22 +47,52 @@ def dolzine(graph):
         s.append(len(bfs_shortest_path(graph, k[0],k[i]))-1)
     return(sum(s))
 
+print(dolzine(graph))
 def bliznjica(graph):
-    m = (0,0,99999999999999)
-    povezave = [] #Seznam že preverjenih povezav
+    m = (0,0,99999999999999) #Trojica bližnjica, skupna razdalja
+    povezave = [] #seznam preverjenih povezav
     for i in range(0,len(k)):
         for j in range(0,len(k)):
-            if k[j] not in graph.get(k[i]) and k[j] != k[i]: 
+            if k[j] not in graph.get(k[i]) and k[j] != k[i]: #če povezave še ni oz različne točke
                 if (k[i],k[j]) not in povezave:
-                    dic = graph
-                    graph[k[i]].append(k[j]) #graf neusmerjen, dodamo povezavo ij in ji
+                    graph[k[i]].append(k[j]) #dodamo nove povezave, graf neusmerjen
                     graph[k[j]].append(k[i])
-                    dol = dolzine(graph) #izračunamo vsoto vseh poti
-                    graph = dic
-                    povezave.append((k[i],k[j])) #dodamo povezave v seznam že preverjenih povezav
+                    dol = dolzine(graph)  #izračunamo nove razdalje
+                    graph[k[i]].remove(k[j])   #izbrišemo bližnjico
+                    graph[k[j]].remove(k[i])                    
+                    povezave.append((k[i],k[j])) #dodamo bližnjico med že preverjene
                     povezave.append((k[j],k[i]))
-                    if dol < m[2]:   #preverimo če jo nove bližnjica boljša
+
+                    if dol < m[2]:   #preverimo če je nova bližnjica boljša
                         m = (k[j],k[i], dol)
     return(m)
 
+                
+    
 print(bliznjica(graph))
+
+#Kodo je treba še izboljšati, samo osnutek
+m2 = (0,0,0,0,99999999999999)
+preverjene = []
+for i in range(0, len(k)):
+    for j in range(0,len(k)):
+                   for s in range(0, len(k)):
+                       for l in range(0,len(k)):
+                           if k[s] not in graph.get(k[i]):
+                               if k[l] not in graph.get(k[j]):
+                                   if k[i] != k[s]:
+                                       if (k[i], k[l],k[s],k[j]) not in preverjene:
+                                           dic = graph
+                                           graph[k[i]].append(k[s])
+                                           graph[k[j]].append(k[l])
+                                           dol = dolzine(graph)
+                                           preverjene.append((k[i],k[j],k[s],k[l]))
+                                           preverjene.append((k[j],k[i],k[s],k[l]))
+                                           preverjene.append((k[i],k[j],k[l],k[s]))
+                                           preverjene.append((k[j],k[i],k[l],k[s]))
+                                           graph = dic
+                                           print(m2)
+                                           if dol < m2[4]:
+                                               m2 = (k[i],k[s],k[j],k[l],dol)
+print(m2)
+print(preverjene)
